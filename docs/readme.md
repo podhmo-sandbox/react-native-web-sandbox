d8717dade272e6f189b3b26a6d00539f67349af7
Naoyoshi Kobayashi <kobayashi@wacul.co.jp>
2018-06-01 17:53:28 +0900

    hmm?

--- docs/readme.md
+++ docs/readme.md
@@ -1,3 +1,3 @@
 - https://github.com/necolas/react-native-web
-
+- https://glitch.com/edit/#!/react-native
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+AppRegistry.registerComponent('App', () => App);
+
+AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root')
+});
